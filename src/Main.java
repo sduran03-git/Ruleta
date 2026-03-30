3bed9100f541cdf66509a51fbb55703140651d4a
sduran03-git <s.duran03@ufromail.cl>
2026-03-29 22:13:36 -0300

    Inicio de ronda

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,14 @@ public class Ruleta {
     public static int historialSize = 0;
     public static Random rng = new Random();
     public static int[] numerosRojos =
-            {1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35};
+            {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35};
     public static int[] numerosNegros =
-            {2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36};}
-/**
- * Método principal: inicia el programa llamando al menú.
- */
+            {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36};
+    }
+
     public static void main(String[] args) {
         menu();
     }
-    /**
-     * Controla el flujo principal del programa mostrando un menú en consola.
-     */
 
     public static void menu() {
         Scanner in = new Scanner(System.in);
@@ -30,9 +26,7 @@ public class Ruleta {
             ejecutarOpcion(opcionElegida, in);
         } while (opcionElegida != 3);
     }
-    /**
-     * Muestra en consola las opciones disponibles del menú.
-     */
+
     public static void mostrarMenu() {
         System.out.println("""
                 Bienvenido al casino Black Cat, por favor selecciona una opcion:
@@ -63,23 +57,30 @@ public class Ruleta {
             System.out.println("Opción inválida. Por favor, intente de nuevo.");
         }
     }
-    /**
-     * Inicia una ronda de la ruleta: leer apuesta, girar, evaluar y mostrar resultado.
-     * @param in Scanner para entrada por consola.
-     */
-    public static void iniciarRonda(Scanner in) {}
-    /**
-     * Permite al usuario seleccionar el tipo de apuesta (R/N/P/I).
-     * @param in Scanner para entrada por consola.
-     * @return el tipo de apuesta elegido.
-     */
-    public static char leerTipoApuesta(Scanner in) {
-        return 0;
+
+    public static void iniciarRonda(Scanner in) {
+        char tipo = leerTipoApuesta(in);
+
+        System.out.print("Ingrese el monto a apostar: $");
+        int monto = in.nextInt();
+        in.nextLine();
+
+        int numero = girarRuleta();
+        boolean acierto = evaluarResultado(numero, tipo);
+
+        mostrarResultado(numero, tipo, monto, acierto);
+        registrarResultado(numero, monto, acierto);
     }
-    /**
-     * Simula el giro de la ruleta generando un número aleatorio de 0 a 36.
-     * @return número de la ruleta.
-     */
+
+    public static char leerTipoApuesta(Scanner in) {
+        System.out.println("Tipos de apuesta: Rojo, Negro, Par, Impar");
+        System.out.print("Ingrese su elección: ");
+        String palabra = in.next().toUpperCase();
+        in.nextLine();
+
+        return palabra.charAt(0);
+    }
+
     public static int girarRuleta() {
         return 0;
     }
